7d1ef2c86a0ccfb62ac6397b98f6c11660800b3d
richo <richo@psych0tik.net>
2012-06-24 18:39:22 +1000

    Debug printing

--- lib/juicy.rb
+++ lib/juicy.rb
@@ -1,12 +1,23 @@
 require 'mongo'
 require 'mongoid'
 module Juicy
+  # TODO
+  def self.debug
+    true
+  end
+
+end
+
+def _debug(txt)
+  if Juicy.debug
+    $stderr.puts txt
+  end
 end
 
 # Load juicy core, followed by extras
 ["", "controllers", "models"].each do |el|
   Dir[File.dirname(__FILE__) + "/juicy/#{el}/*.rb"].each  do |file|
-    puts "Loading #{file}"
+    _debug "Loading #{file}"
     require file
   end
 end
