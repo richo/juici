067b14b9baddfeffb203ce9e55855e530bccfda4
Rich Healey <richard.healey@99designs.com>
2012-06-29 15:49:53 +1000

    Cconvert buildthread to a mixin

---
+++ lib/juicy/build_logic.rb
@@ -0,0 +1,26 @@
+require 'fileutils'
+require 'tempfile'
+module Juicy
+   module BuildLogic
+
+    def spawn_build
+      raise "No such work tree" unless FileUtils.mkdir_p(worktree)
+      spawn(command, worktree)
+    end
+
+  private
+
+    def spawn(cmd, dir)
+      @buffer = Tempfile.new('juicy-xxxx')
+      $stderr.puts "cmd: #{cmd}"
+      $stderr.puts "dir: #{dir}"
+      Process.spawn(environment, cmd,
+        :chdir => dir,
+        :in  => "/dev/null",
+        :out => @buffer.fileno,
+        :err => [:child, :out]
+      )
+    end
+
+  end
+end
