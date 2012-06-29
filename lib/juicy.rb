6adc0f36ba19792f3d06490a64f6667d7ef2f7d8
Rich Healey <richard.healey@99designs.com>
2012-06-29 11:20:21 +1000

    Clean up debug hook (slightly)

--- lib/juicy.rb
+++ lib/juicy.rb
@@ -1,23 +1,15 @@
 require 'mongo'
 require 'mongoid'
 module Juicy
-  # TODO
-  def self.debug
-    true
-  end
-
-end
-
-def _debug(txt)
-  if Juicy.debug
-    $stderr.puts txt
+  def self.dbgp(*args)
+    ENV['JUICY_DEBUG'] && $stderr.puts(args)
   end
 end
 
 # Load juicy core, followed by extras
 ["", "controllers", "models"].each do |el|
   Dir[File.dirname(__FILE__) + "/juicy/#{el}/*.rb"].each  do |file|
-    _debug "Loading #{file}"
+    Juicy.dbgp "Loading #{file}"
     require file
   end
 end
