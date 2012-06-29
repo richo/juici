187aaff6e2ea7500cca1f93e3854bd060d7b6a4a
Rich Healey <richard.healey@99designs.com>
2012-06-29 12:11:23 +1000

    Add enough to start some specs

---
+++ spec/spec_helper.rb
@@ -0,0 +1,13 @@
+require 'juicy'
+require 'mocha'
+
+require 'fileutils'
+
+Dir["#{File.expand_path(File.dirname(__FILE__))}/helpers/**/*.rb"].each do |f|
+  puts "Requiring #{f}"
+  require f
+end
+
+RSpec.configure do |config|
+  config.mock_framework = :mocha
+end
