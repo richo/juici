ce0d74d2d38467ca94ade71cf419cdaec3b06c12
Rich Healey <richard.healey@99designs.com>
2012-06-29 15:51:00 +1000

    Check we can creat child processes

--- spec/build_process_spec.rb
+++ spec/build_process_spec.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe "Juicy build abstraction" do
 
   before(:all) do
-    @app = Juicy::App.new
+    @app = Juicy::App.new(workers: 0)
   end
 
   after(:all) do
@@ -12,11 +12,16 @@ describe "Juicy build abstraction" do
 
 
   it "Should run a given command in a subshell" do
+    worker = Juicy::Watcher.start!
     build = Juicy::Build.new(parent: "test project",
                       environment: {},
                       command: "echo 'test build succeeded'")
     $build_queue << build
-    # Deliberately don't push to mongo
-    # build.save!
+    # Wait a reasonable time for build to finish
+    # TODO: This can leverage the hooks system
+    # TODO: Easer will be to have worker.block or something
+    sleep 5
+    build[:status].should == :success
   end
+
 end
