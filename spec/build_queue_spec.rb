a460776aaa89122192d1ec599e7304019221daa2
Rich Healey <richard.healey@99designs.com>
2012-06-29 12:29:58 +1000

    Test priority system in build_queue

---
+++ spec/build_queue_spec.rb
@@ -0,0 +1,37 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe Juicy::BuildQueue do
+
+  subject { Juicy::BuildQueue.new.tap { |q| q.__builds = @builds } }
+
+  it "Should return 1 as min priority when empty" do
+    @builds = []
+    subject.current_min_priority.should == 1
+  end
+
+  it "Should return the min priority when set" do
+    @builds = builds_with(priority: [ 5, 3, 7 ])
+    subject.current_min_priority.should == 3
+  end
+
+  it "Should deal gracefully with nil" do
+    @builds = builds_with(priority: [ 5, 4, nil,  7 ])
+    subject.current_min_priority.should == 4
+  end
+
+end
+
+class Juicy::BuildQueue #{{{ Test injection
+  def __builds=(builds)
+    @builds = builds
+  end
+end #}}}
+
+def builds_with(args)
+  args.map do |k, v|
+    v.map do |i|
+      stub(k => i)
+    end
+  end.flatten
+end
+
