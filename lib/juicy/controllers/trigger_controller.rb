4b60d2008a2221d6d13aea417f106857754eaef9
richo <richo@psych0tik.net>
2012-08-19 13:19:31 +1000

    Include a priority field in the build screen

--- lib/juicy/controllers/trigger_controller.rb
+++ lib/juicy/controllers/trigger_controller.rb
@@ -16,16 +16,31 @@ module Juicy
         end
 
         build[:environment] = environment.to_hash
+        build[:priority] = build_priority
 
         build.save!
         $build_queue << build
       end
     end
 
+    # These accessors are not really the concern of a "Controller". On the
+    # other hand, this thing is also not a controller realistically, so I'm
+    # just going to carry on using this as a mechanism to poke at my model.
+    #
+    # I later plan to implement a more strictly bounded interface which bails
+    # instead of coercing.
     def build_command
       # TODO Throw 422 not acceptable if missing
       params['command'].tap do |c|
         raise "No command given" if c.nil?
+      end
+    end
+
+    def build_priority
+      if params['priority'] =~ /^[0-9]+$/
+        params['priority'].to_i
+      else
+        1
       end
     end
 
