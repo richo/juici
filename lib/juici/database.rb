2324d8886b511fc7e8c20576e9b3eefa925dcaf8
richo <richo@psych0tik.net>
2012-10-08 07:49:46 +1100

    Make juici/database self contained

--- lib/juici/database.rb
+++ lib/juici/database.rb
@@ -1,19 +1,23 @@
-class Juici::Database
-  class << self
-    def mongoid_config_file
-      %w[mongoid.yml mongoid.yml.sample].each do |file|
-        path = File.join("config", file)
-        return path if File.exist?(path)
-      end
-      raise "No database config file"
-    end
-
-    def initialize!
-      if ENV['RACK_ENV'] == "development"
-        Mongoid.logger.level = Logger::INFO
+require 'mongo'
+require 'mongoid'
+module Juici
+  class Database
+    class << self
+      def mongoid_config_file
+        %w[mongoid.yml mongoid.yml.sample].each do |file|
+          path = File.join("config", file)
+          return path if File.exist?(path)
+        end
+        raise "No database config file"
       end
 
-      Mongoid.load!(mongoid_config_file)
+      def initialize!
+        if ENV['RACK_ENV'] == "development"
+          Mongoid.logger.level = Logger::INFO
+        end
+
+        Mongoid.load!(mongoid_config_file)
+      end
     end
   end
 end
