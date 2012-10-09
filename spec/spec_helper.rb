3f51860c1b6ee28f3c1c4ea57ce9107b3e50a715
richo <richo@psych0tik.net>
2012-10-09 15:24:57 +1100

    Set RACK_ENV before juici has a chance to

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,9 @@
+ENV['RACK_ENV'] ||= "test"
+
 require 'juici'
 require 'mocha'
 
 require 'fileutils'
-
-ENV['RACK_ENV'] ||= "test"
 
 Dir["#{File.expand_path(File.dirname(__FILE__))}/helpers/**/*.rb"].each do |f|
   puts "Requiring #{f}"
