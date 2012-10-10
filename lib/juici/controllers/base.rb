c5b00aefc040ea627cadf38038c0b6d2f81e747c
richo <richo@psych0tik.net>
2012-10-10 23:48:53 +1100

    Wire up #styles to controllers

--- lib/juici/controllers/base.rb
+++ lib/juici/controllers/base.rb
@@ -1,6 +1,21 @@
 module Juici
   module Controllers
     class Base
+
+      def build_opts(opts)
+        default_opts.merge(opts)
+      end
+
+      def default_opts
+        {
+          :styles => styles
+        }
+      end
+
+      def styles
+        []
+      end
+
     end
   end
 end
--- lib/juici/controllers/builds.rb
+++ lib/juici/controllers/builds.rb
@@ -24,7 +24,7 @@ module Juici::Controllers
         limit(::Juici::Config.builds_per_page).
         skip(params[:page].to_i * ::Juici::Config.builds_per_page)
 
-      yield [:"builds/list", {:project => project, :builds => builds, :pages => pages}]
+      yield [:"builds/list", build_opts({:project => project, :builds => builds, :pages => pages})]
     end
 
     def show
@@ -33,7 +33,11 @@ module Juici::Controllers
       project = ::Juici::Project.where(name: params[:project]).first
       build   = ::Juici::Build.where(parent: project.name, _id: params[:id]).first
       # return 404 unless project && build
-      yield [:"builds/show", {:project => project, :build => build}]
+      yield [:"builds/show", build_opts({:project => project, :build => build})]
+    end
+
+    def styles
+      ["builds"]
     end
 
   end
