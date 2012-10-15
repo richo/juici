2c93c0ce1449666ef3ea4a9a816744a2151ea29d
richo <richo@psych0tik.net>
2012-10-15 14:57:13 +1100

    Make sure that we get builds in increasing order
    (by doc id)

--- lib/juici/controllers/builds.rb
+++ lib/juici/controllers/builds.rb
@@ -19,6 +19,7 @@ module Juici::Controllers
 
       project = ::Juici::Project.where(name: params[:project]).first
       builds  = ::Juici::Build.where(parent: project.name).
+        asc(:_id).
         limit(::Juici::Config.builds_per_page).
         skip(params[:page].to_i * ::Juici::Config.builds_per_page)
 
