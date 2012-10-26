308ed36b63de5995323f6f70a5a683eefad15084
richo <richo@psych0tik.net>
2012-10-26 15:54:58 +1100

    Wire up a build queue

---
+++ lib/juici/controllers/build_queue.rb
@@ -0,0 +1,14 @@
+module Juici::Controllers
+  class BuildQueue < Base
+
+    def list
+      builds = $build_queue.builds.sort_by(&:priority)
+      yield [:"queue/list", opts(:builds => builds)]
+    end
+
+    def opts(_opts={})
+      {:active => :queue}.merge(_opts)
+    end
+
+  end
+end
