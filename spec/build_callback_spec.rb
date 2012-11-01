c16fe024119694da38045bbc6be2fab3ef993913
richo <richo@psych0tik.net>
2012-11-01 12:27:26 +1100

    Refactor Callbacks, add specs

--- spec/build_callback_spec.rb
+++ spec/build_callback_spec.rb
@@ -3,7 +3,22 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 describe "Juicy::Build::Callback" do
 
   it "should call #process! on each of it's callbacks" do
-    pending("Not yet specced")
+    def mock_callback
+      mock(:payload= => nil).tap do |callback|
+        callback.expects(:process!).once
+      end
+    end
+
+    callbacks = ["rawr", "test"]
+
+    callbacks.each do |callback|
+      Juici::Callback.stubs(:new).with(callback).
+        returns(mock_callback)
+    end
+
+    build = Juici::Build.new(:callbacks => callbacks)
+    build.start!
+    build.success!
   end
 
 end
