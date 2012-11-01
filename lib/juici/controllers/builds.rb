abaa6cdd23222b40e9a216e2a2555002a2d44dfd
richo <richo@psych0tik.net>
2012-11-01 21:18:07 +1100

    Use bootstrap buttons for pagination

--- lib/juici/controllers/builds.rb
+++ lib/juici/controllers/builds.rb
@@ -3,14 +3,6 @@ module Juici::Controllers
 
     def list
       params[:page] = params[:page] ? params[:page].to_i : 0
-      pages = {}
-
-      if params[:page] > 0
-        pages[:prev] = params[:page] - 1
-      end
-      #if lol
-      pages[:next] = params[:page] + 1
-      #end
 
       unless project = ::Juici::Project.where(name: params[:project]).first
         not_found
@@ -20,6 +12,9 @@ module Juici::Controllers
         desc(:_id).
         limit(::Juici::Config.builds_per_page).
         skip(params[:page].to_i * ::Juici::Config.builds_per_page)
+
+      # XXX Extract this into Juici::Pages or somesuch.
+      pages = [params[:page], (builds.count.to_f / ::Juici::Config.builds_per_page).ceil]
 
       yield [:"builds/list", build_opts({:project => project, :builds => builds, :pages => pages})]
     end
