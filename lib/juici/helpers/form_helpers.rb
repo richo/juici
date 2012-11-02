6f52aabd53135879ab64bbf93922843c859afcab
richo <richo@psych0tik.net>
2012-11-02 15:09:23 +1100

    Remove well class from forms

--- lib/juici/helpers/form_helpers.rb
+++ lib/juici/helpers/form_helpers.rb
@@ -1,6 +1,6 @@
 def form_at(route, fields, opts={})
   form = ""
-  form << %Q{<form class="well" action="#{route}" method="post">\n}
+  form << %Q{<form action="#{route}" method="post">\n}
 
   fields.each do |name, value|
     form << %Q{<input type="hidden" name="#{name}" value="#{value}">\n}
