d8eeebbfad90621016216b3729ec24510d8b6637
richo <richo@psych0tik.net>
2012-11-21 13:29:50 +1100

    Make the form helper more flexible

--- lib/juici/helpers/form_helpers.rb
+++ lib/juici/helpers/form_helpers.rb
@@ -1,11 +1,15 @@
 def form_at(route, fields, opts={})
   form = ""
-  form << %Q{<form action="#{route}" method="post">\n}
+  form << %Q{<form action="#{route}" method="#{opts[:method] || "post"}">\n}
 
   fields.each do |name, value|
     form << %Q{<input type="hidden" name="#{name}" value="#{value}">\n}
   end
 
-  form << %Q{<button type="submit" class="btn">#{opts[:submit] || "submit"}</button>}
+  yield form if block_given?
+
+  unless opts[:no_submit]
+    form << %Q{<button type="submit" class="btn">#{opts[:submit] || "submit"}</button>}
+  end
   form << %Q{</form>}
 end
