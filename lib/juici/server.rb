0d0dce6016a052aec9601e8170987369a083b9d4
richo <richo@psych0tik.net>
2012-11-29 13:56:21 +1100

    Use regexes in routes to avoid breaking on spaces.
    Update #53

--- lib/juici/server.rb
+++ lib/juici/server.rb
@@ -71,15 +71,10 @@ module Juici
       erb(:redirect, {}, {})
     end
 
-    post '/builds/:project/rebuild/:id' do
-      build = Controllers::Trigger.new(params[:project], params).rebuild!
-      @redirect_to = build_url_for(build)
-      erb(:redirect, {}, {})
-    end
-
-    post '/builds/:user/:project/rebuild/:id' do
-      params[:project] = "#{params[:user]}/#{params[:project]}"
-      build = Controllers::Trigger.new(params[:project], params).rebuild!
+    post %r{^/builds/(?<project>\w+)/rebuild/(?<id>[^/]*)$} do |project, id|
+      params[:project] = project
+      params[:id] = id
+      build = Controllers::Trigger.new(project, params).rebuild!
       @redirect_to = build_url_for(build)
       erb(:redirect, {}, {})
     end
@@ -102,59 +97,32 @@ module Juici
       end
     end
 
-    get '/builds/:project/list' do
+    get %r{^/builds/(?<project>\w+)/list$} do |project|
+      params[:project] = project
       Controllers::Builds.new(params).list do |template, opts|
         erb(template, {}, opts)
       end
     end
 
-    get '/builds/:user/:project/list' do
-      params[:project] = "#{params[:user]}/#{params[:project]}"
-      Controllers::Builds.new(params).list do |template, opts|
-        erb(template, {}, opts)
-      end
-    end
-
-    get '/builds/:project/edit/:id' do
+    get %r{^/builds/(?<project>\w+)/edit/(?<id>[^/]*)$} do |project, id|
       Controllers::Builds.new(params).edit do |template, opts|
         erb(template, {}, opts)
       end
     end
 
-    get '/builds/:user/:project/edit/:id' do
-      params[:project] = "#{params[:user]}/#{params[:project]}"
-      Controllers::Builds.new(params).edit do |template, opts|
-        erb(template, {}, opts)
-      end
-    end
-
-    post '/builds/:project/edit/:id' do
+    post %r{^/builds/(?<project>\w+)/edit/(?<id>[^/]*)$} do |project, id|
       build = Controllers::Builds.new(params).update!
       @redirect_to = build_url_for(build)
       erb(:redirect, {}, {})
     end
 
-    post '/builds/:user/:project/edit/:id' do
-      params[:project] = "#{params[:user]}/#{params[:project]}"
-      build = Controllers::Builds.new(params).update!
-      @redirect_to = build_url_for(build)
-      erb(:redirect, {}, {})
-    end
-
-    get '/builds/:project/show/:id' do
+    get %r{^/builds/(?<project>\w+)/show/(?<id>[^/]*)$} do |project, id|
       Controllers::Builds.new(params).show do |template, opts|
         erb(template, {}, opts)
       end
     end
 
-    get '/builds/:user/:project/show/:id' do
-      params[:project] = "#{params[:user]}/#{params[:project]}"
-      Controllers::Builds.new(params).show do |template, opts|
-        erb(template, {}, opts)
-      end
-    end
-
-    post '/trigger/:project' do
+    post %r{^/trigger/(?<project>\w+)$} do |project, id|
       Controllers::Trigger.new(params[:project], params).build!
     end
 
