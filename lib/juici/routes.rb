4c4951d7082af7b0173c109c72ae339c9d196f7c
richo <richo@psych0tik.net>
2012-12-17 17:46:58 +1100

    Handle spaces and dots in project names

--- lib/juici/routes.rb
+++ lib/juici/routes.rb
@@ -1,33 +1,34 @@
 module Juici
   module Router
     include Routes
+    URL_PART = /[\w\/. %]+/
 
     def build_new_path
       "/builds/new"
     end
 
     def build_list_path
-      %r{^/builds/(?<project>[\w\/]+)/list$}
+      %r{^/builds/(?<project>#{URL_PART})/list$}
     end
 
     def build_rebuild_path
-      %r{^/builds/(?<project>[\w\/]+)/rebuild/(?<id>[^/]*)$}
+      %r{^/builds/(?<project>#{URL_PART})/rebuild/(?<id>[^/]*)$}
     end
 
     def build_edit_path
-      %r{^/builds/(?<project>[\w\/]+)/edit/(?<id>[^/]*)$}
+      %r{^/builds/(?<project>#{URL_PART})/edit/(?<id>[^/]*)$}
     end
 
     def build_show_path
-      %r{^/builds/(?<project>[\w\/]+)/show/(?<id>[^/]*)$}
+      %r{^/builds/(?<project>#{URL_PART})/show/(?<id>[^/]*)$}
     end
 
     def build_output_path
-      %r{^/builds/(?<project>[\w\/]+)/(?<id>[^/]*)/_output$}
+      %r{^/builds/(?<project>#{URL_PART})/(?<id>[^/]*)/_output$}
     end
 
     def build_trigger_path
-      %r{^/trigger/(?<project>[\w\/]+)$}
+      %r{^/trigger/(?<project>#{URL_PART})$}
     end
 
   end
